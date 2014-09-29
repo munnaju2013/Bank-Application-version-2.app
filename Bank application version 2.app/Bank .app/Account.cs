4f08d1f78db5c5a4e91073ad9e66b84c96d6e408
munnaju2013 <munnaju2013@gmail.com>
2014-09-29 15:03:01 +0600

    add file

---
+++ Bank application version 2.app/Bank .app/Account.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.app
+{
+    class Account
+    {
+        public string number;
+        public string name;
+        public double balance;
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            balance -= amount;
+
+        }
+
+        public string GetReport()
+        {
+            string msg = name + " your account no is " + number + " and balance is " + balance;
+            return msg;
+        }
+           
+    }
+}
---
+++ Bank application version 2.app/Bank .app/BankUI.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bank.app
+{
+    public partial class BankUI : Form
+    {
+        public BankUI()
+        {
+            InitializeComponent();
+        }
+
+        private Account anAccount;
+        private double amount;
+
+        private void createButton_Click(object sender, EventArgs e)
+        {
+            anAccount = new Account();
+            anAccount.number = accountNumberTextBox.Text;
+            anAccount.name = customerNameTextBox.Text;
+            MessageBox.Show(@"Account has been been created");
+        }
+
+        private void depositButton_Click(object sender, EventArgs e)
+        {
+            if (anAccount != null)
+            {
+                amount = Convert.ToDouble(amountTextBox.Text);
+                anAccount.Deposit(amount);
+                MessageBox.Show(@"Account has been deposited....");
+                amountTextBox.Text=String.Empty;
+
+            }
+            else
+            {
+                MessageBox.Show(@"Please create a new account...");
+            }
+        }
+
+        private void withdrawButton_Click(object sender, EventArgs e)
+        {
+            amount = Convert.ToDouble(amountTextBox.Text);
+            anAccount.Withdraw(amount);
+            MessageBox.Show(@"Amount has been withdrawn......");
+            amountTextBox.Text = String.Empty;
+        }
+
+        private void reportButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(anAccount.GetReport());
+        }
+
+    }
+}
